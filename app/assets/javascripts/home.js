c3d22c52f44ff8a107e1076cb17d214eb90eb9be
Allison Goldfarb <allisonrgoldfarb@gmail.com>
2015-11-14 16:52:37 -0600

    styling

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,16 +1,15 @@
 $(document).ready( function() {
-  $('#about_nav').click(function() {
-    $('#about').fadeIn('slow',function(){
-      $('#contact_nav').click(function() {
-        $('#about').fadeOut('slow');
-      });
-    });
-  });
-  $('#contact_nav').click(function(){
-    $('#contact').fadeIn('slow',function(){
-      $('#about_nav').click(function(){
-        $('#contact').fadeOut('slow');
-      })
+  //  listen for clicks on .navbar_link
+  $('.navigation_link').click(function() {
+    //  grab ID value of the clicked element
+    var linkId = $(this).attr('id');
+    // select corresponding box
+    var box = $('.detailBox#' + linkId);
+
+    //  hide all other boxes...
+    $('.detailBox').not(box).fadeOut("fast",function(){
+      //...and then show this one after
+      box.fadeIn('fast');
     });
   });
 });
